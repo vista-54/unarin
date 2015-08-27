1afb44b87a2c2058dc1702ca52b6f8d634ee409e
vista <vista-54@ukr.net>
2015-08-27 17:59:04 +0300

    2708

--- src/android/LocationManager.java
+++ src/android/LocationManager.java
@@ -79,7 +79,7 @@ public class LocationManager extends CordovaPlugin implements BeaconConsumer {
     //listener for changes in state for system Bluetooth service
 	private BroadcastReceiver broadcastReceiver; 
 	private BluetoothAdapter bluetoothAdapter;
-	private beaconUtill = new BeaconServiceUtility(this);
+	
 
     /**
      * Constructor.
